5d11dbf0fa9a553c341bf36699eb5c2b2cd67a71
rjarrin <jarrin.ricardo@gmail.com>
2024-03-20 19:48:20 -0400

    Header layout

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Hold the library objects in array
+const myLibrary = [];
+
+function Book(author, title, pages, readStatus, type) {
+    // Constructor elements
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    // eBook, Book, Graphic Novel, Picture Book, Early Reader
+    this.type = type; 
+    this.readStatus = readStatus;
+    // Toogle read status
+    this.toggleRead = function () {
+        console.log("Before:" + this.readStatus);
+        this.readStatus = !this.readStatus;
+        console.log("After:" + this.readStatus);
+    }
+}
+
+function Audiobook() {
+    // Streaming, Downloadable, CD
+    this.type = type;
+}
+
+function MoviesTV() {
+    // Streaming, DVD, Blu-ray
+    this.type = type;
+}
+
+function Software() {
+    // Video Game
+    this.type = type;
+}
+
+
+function addToLibrary() {
+    // do stuff here
+}
+
